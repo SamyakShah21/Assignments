c9d16d6bb6daf74280a668a31d2337768a8907c4
Samyak Shah <samyakshah21@gmail.com>
2020-10-13 11:49:50 +0530

    Added Comments to Assignment 6E

--- Assignment 6E.py
+++ Assignment 6E.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 def computeLanguageError(freq):
     error=np.zeros(15)
-    data=pd.read_csv("letter_frequencies.csv")
+    data=pd.read_csv("letter_frequencies.csv")	            #Read as a dataframe
     for i in range(1,16,1):                                 #start,stop,step
         for j in range(26):
             error[i-1]=error[i-1]+(data.iloc[j][i]-freq[j])**2
